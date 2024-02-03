0541a50b714963f1fded213ca107f65d3221d557
jaber.abbas <jaber.h.abbas@gmail.com>
2024-02-03 22:42:50 +0100

    test.json was not added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,32 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'my-app';
+  url = `http://echo.jsontest.com/key/value/one/two`;
+  jsonPath = "./assets/test.json";
+  items: any[] = [];
+  jsonContent: any;
+  constructor(private http: HttpClient) {
+    this.httpClientCall();
+  }
+
+  private httpClientCall(): void{
+    this.http.get(this.url).toPromise().then(
+      (data: any) => {
+        console.log("after jsontest call");
+        console.log("data to json: " + JSON.stringify(data));
+        this.jsonContent = JSON.stringify(data);
+        
+        for (let fieldName in data)
+          if (data.hasOwnProperty(fieldName))
+            this.items.push(data[fieldName]);
+      }
+    ).catch(error=>console.error("err catched: " + error));
+  
+  }
 }
