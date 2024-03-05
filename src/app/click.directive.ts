aaee1a2472dd726ee3c1d7d2c59e56e5f2463ea5
jaber.abbas <jaber.h.abbas@gmail.com>
2024-03-05 22:05:24 +0100

    first commit

---
+++ src/app/click.directive.ts
@@ -0,0 +1,37 @@
+import { Directive, ElementRef, EventEmitter, Output } from '@angular/core';
+
+@Directive({
+  standalone: true,
+  selector: '[myClick]'
+})
+export class ClickDirective {
+  @Output('myClick') clicks = new EventEmitter<string>(); //  @Output(alias) propertyName = ...
+
+  toggle = false;
+
+  constructor(el: ElementRef) {
+    el.nativeElement
+      .addEventListener('click', (event: Event) => {
+        this.toggle = !this.toggle;
+        this.clicks.emit(this.toggle ? 'Click!' : '');
+      });
+  }
+}
+
+@Directive({
+  standalone: true,
+  selector: '[myClick2]',
+  outputs: ['clicks:myClick']  // propertyName:alias
+})
+export class ClickDirective2 {
+  clicks = new EventEmitter<string>();
+  toggle = false;
+
+  constructor(el: ElementRef) {
+    el.nativeElement
+      .addEventListener('click', (event: Event) => {
+        this.toggle = !this.toggle;
+        this.clicks.emit(this.toggle ? 'Click2!' : '');
+      });
+  }
+}
