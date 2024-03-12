1fb5878552ce4a677932e29e528d79685762d472
jaber.abbas <jaber.h.abbas@gmail.com>
2024-03-12 22:07:37 +0100

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
-import { Routes } from '@angular/router';
+import {Routes, UrlSegment} from '@angular/router';
 
-export const routes: Routes = [];
+import {ProfileComponent} from './profile/profile.component';
+
+export const routes: Routes = [
+  {
+    matcher: (url) => {
+      if (url.length === 1 && url[0].path.match(/^@[\w]+$/gm)) {
+        return {consumed: url, posParams: {username: new UrlSegment(url[0].path.slice(1), {})}};
+      }
+
+      return null;
+    },
+    component: ProfileComponent
+  },
+];
