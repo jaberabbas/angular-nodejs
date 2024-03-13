e7da8730bf60942975b3dfb79a8eea97c86b6947
jaber.abbas <jaber.h.abbas@gmail.com>
2024-03-13 15:44:41 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,55 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+import { NameEditorComponent } from './name-editor/name-editor.component';
+import { ProfileEditorComponent } from './profile-editor/profile-editor.component';
+
+export type EditorType = 'name' | 'profile';
 
 @Component({
-  selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  selector: 'app-root',
+  template: `
+    <h1>Reactive Forms</h1>
+
+    <nav>
+      <button type="button" (click)="toggleEditor('name')">Name Editor</button>
+      <button type="button" (click)="toggleEditor('profile')">
+        Profile Editor
+      </button>
+    </nav>
+
+    <app-name-editor *ngIf="showNameEditor"></app-name-editor>
+
+    <app-profile-editor *ngIf="showProfileEditor"></app-profile-editor>
+  `,
+  styles: [
+    `
+      nav button {
+        padding: 1rem;
+        font-size: inherit;
+      }
+    `,
+  ],
+  imports: [
+    NameEditorComponent,
+    ProfileEditorComponent,
+    NgIf,
+    ReactiveFormsModule,
+  ],
 })
 export class AppComponent {
-  title = 'reactive-forms';
+  editor: EditorType = 'name';
+
+  get showNameEditor() {
+    return this.editor === 'name';
+  }
+
+  get showProfileEditor() {
+    return this.editor === 'profile';
+  }
+
+  toggleEditor(type: EditorType) {
+    this.editor = type;
+  }
 }
