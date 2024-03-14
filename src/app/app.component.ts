15f9c798806b5022ea3a39d36124701bef82404a
jaber.abbas <jaber.h.abbas@gmail.com>
2024-03-14 11:44:55 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HeroFormReactiveComponent } from './reactive/hero-form-reactive.component';
+import { HeroFormTemplateComponent } from './template/hero-form-template.component';
 
 @Component({
   selector: 'app-root',
+  template: `
+    <h1>Form validation example</h1>
+    <app-hero-form-template></app-hero-form-template>
+    <hr />
+    <app-hero-form-reactive></app-hero-form-reactive>
+  `,
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [HeroFormTemplateComponent, HeroFormReactiveComponent],
 })
-export class AppComponent {
-  title = 'form-validation';
-}
+export class AppComponent {}
